e36d647fbb3d16708db17357bee26e94ccc3e22e
NIGUNKARTHI <154399216+NIGUNKARTHI@users.noreply.github.com>
2025-11-02 11:59:01 +0530

    backend done

---
+++ server.js
@@ -0,0 +1,27 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+const campaignRoutes = require('./router/campaignRoutes');
+const authRoutes = require('./router/authRoutes');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+app.use(cors());
+
+app.use('/campaign', campaignRoutes);
+app.use('/auth', authRoutes);
+
+mongoose.connect("mongodb://localhost:27017/sourcecrowd")
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(9000, () => {
+      console.log(`Server running at http://localhost:9000`);
+    });
+  })
+  .catch(err => console.log('DB connection error:', err));
+
