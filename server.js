1ba8b09988fcc60636f443a5121ab15c16ce001e
NIGUNKARTHI <154399216+NIGUNKARTHI@users.noreply.github.com>
2025-11-02 12:19:03 +0530

    cors updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const authRoutes = require('./router/authRoutes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
-app.use(cors());
+app.use(cors('*'));
 
 app.use('/campaign', campaignRoutes);
 app.use('/auth', authRoutes);
